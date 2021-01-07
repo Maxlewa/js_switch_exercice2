9a331286627ae19dcb38dbfb9bf115455c605a4a
Maxence <lemairemaxence@hotmail.com>
2021-01-07 12:23:51 +0100

    exo ok

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+// EXO 1
+
+// let nb = prompt("Donne-moi un nombre")
+
+// switch (true) {
+//     case (nb%2 == 0):
+//         alert("Ce nombre est pair")
+//         break;
+
+//     default:
+//         alert("Ce nombre est impair")
+//         break;
+// }
+
+// EXO 2
+
+let douze = prompt("Un nombre entre 1 et 12 stp :")
+
+switch (douze) {
+    case "1":
+        alert("Janvier")
+        break;
+    case "2":
+        alert("Février")
+        break;
+    case "3":
+        alert("Mars")
+        break;
+    case "4":
+        alert("Avril")
+        break;
+    case "5":
+        alert("Mai")
+        break;
+    case "6":
+        alert("Juin")
+        break;
+    case "7":
+        alert("Juillet")
+        break;
+    case "8":
+        alert("Août")
+        break;
+    case "9":
+        alert("Septembre")
+        break;
+    case "10":
+        alert("Octobre")
+        break;
+    case "11":
+        alert("Novembre")
+        break;
+    case "12":
+        alert("Décembre")
+        break;
+
+    default:
+        alert("Entre 1 et 12 on a dit ...")
+        break;
+}
